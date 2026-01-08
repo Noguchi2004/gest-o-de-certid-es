819f6351e53ff663c3497f6b8343561e2c7c9728
Noguti <142026294+Noguchi2004@users.noreply.github.com>
2026-01-08 10:44:59 -0300

    Update certidao.ts

--- api/certidao.ts
+++ api/certidao.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 const APPS_SCRIPT_URL =
-  'https://script.google.com/macros/s/AKfycbyWuNyybudc18dO83VPjdVWEujBK_Sm9YxUDbombhkJjGkSUCWc3asdGDPgznYdMlN6mQ/exec';
+  'https://script.google.com/macros/s/AKfycbxMh707Oq-U0NFflYNpcQKT662hC-aMeDKIIwClr-14fAs63JcQ5BYO39CMCQLPVNmTXg/exec';
 
 export default async function handler(
   req: VercelRequest,
