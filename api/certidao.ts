de99043b6928d6a26a8c68587cdcad688517140e
Noguti <142026294+Noguchi2004@users.noreply.github.com>
2026-01-08 09:23:59 -0300

    Create certidao.ts

---
+++ api/certidao.ts
@@ -0,0 +1,34 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+const APPS_SCRIPT_URL =
+  'https://script.google.com/macros/s/AKfycbwS1iKIXPn_WV578VuXwVS9CQpYmQKSNEvOn3qQZHoNxhiBa9iP054pFc4PvvtqZBsjEA/exec'; // termina com /exec
+
+export default async function handler(
+  req: VercelRequest,
+  res: VercelResponse
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ ok: false, error: 'Method not allowed' });
+  }
+
+  try {
+    const { certidao } = req.body;
+
+    const gsResponse = await fetch(APPS_SCRIPT_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ certidao }),
+    });
+
+    const data = await gsResponse.json();
+
+    return res.status(200).json(data);
+  } catch (error: any) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ ok: false, error: 'Erro ao falar com Apps Script' });
+  }
+}
